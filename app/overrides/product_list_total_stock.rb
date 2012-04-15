d3f82ef097d95843e92b7326bdb33c0efdb92962
Can Tonic <can@cantonic.com>
2012-04-16 00:49:25 +0300

    changed code for getting the total stock

--- app/overrides/product_list_total_stock.rb
+++ app/overrides/product_list_total_stock.rb
@@ -1,8 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/index",
                      :name => "product_list_total_stock",
                      :insert_after => "code[erb-loud]:contains('t(:listing_products)')",
-                     :text => "<code erb-silent>
-																@total_on_hand=0
-																Spree::Product.all.map {|p| @total_on_hand += p.on_hand}
-															</code>
-															(<code erb-loud>@total_on_hand</code> total on hand)")
+                     :text => " (<code erb-loud>Spree::Product.sum(:count_on_hand)</code> total on hand)")
