1eb0ea1490bd0a5a7f29ca2147e158340fbf567f
Can Tonic <can@cantonic.com>
2012-04-19 02:12:34 +0300

    added total cost price

--- app/overrides/product_list_total_stock.rb
+++ app/overrides/product_list_total_stock.rb
@@ -1,4 +1,9 @@
+include ActionView::Helpers::NumberHelper
+
 Deface::Override.new(:virtual_path => "spree/admin/products/index",
                      :name => "product_list_total_stock",
-                     :insert_after => "code[erb-loud]:contains('t(:listing_products)')",
-                     :text => " (<code erb-loud>Spree::Product.sum(:count_on_hand)</code> total on hand)")
+                     :insert_after => "code[erb-loud]:contains('t(:listing_products)')") do		
+	total_on_hand = Spree::Variant.sum(:count_on_hand)
+	total_cost_price = Spree::Variant.sum(:cost_price) * total_on_hand
+	" (#{total_on_hand} total stock | #{number_to_currency total_cost_price} total cost)"
+end
